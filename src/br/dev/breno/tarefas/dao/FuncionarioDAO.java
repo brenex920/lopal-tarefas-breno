67bbc075f5a45c04c90021137a9a5ac191178a3f
BRENO DIAS MACHADO <25132475@dsn127m1246729.cfp127aluno.local>
2025-06-18 14:47:38 -0300

    dao funcionario esta finalizado

--- src/br/dev/breno/tarefas/dao/FuncionarioDAO.java
+++ src/br/dev/breno/tarefas/dao/FuncionarioDAO.java
@@ -1,10 +1,13 @@
 package br.dev.breno.tarefas.dao;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.dev.breno.tarefas.pastas.Pastas;
 import br.dev.breno.tarefas.model.Funcionario;
 
 public class FuncionarioDAO {
@@ -16,31 +19,58 @@ public class FuncionarioDAO {
     private FileReader fr;
     private BufferedReader br;
 
-    public FuncionarioDAO(Funcionario funcionario) throws FileNotFoundException {
-        this.funcionario = funcionario;
-        try {
-            fw = new FileWriter(arquivo, true);
-            bw = new BufferedWriter(fw);
-            fr = new FileReader(arquivo);
-            br = new BufferedReader(fr);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+    public FuncionarioDAO(Funcionario funcionario) {
+		this.funcionario = funcionario;
+		try {
+			fw = new FileWriter(arquivo, true);
+			bw = new BufferedWriter(fw);
+			fr = new FileReader(arquivo);
+			br = new BufferedReader(fr);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
 
-    }
-    public void salvar(){
-        try {
-            bw.write(funcionario.toString());
-            bw.flush();
-            System.out.println(funcionario.getNome() + " gravado com sucelso!");
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+	public void salvar() {
+		try {
+			bw.write(funcionario.toString());
+			bw.flush();
+			System.out.println(funcionario.getNome() + " gravado com sucelso!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Funcionario> getFuncionarios() {
+	    List<Funcionario> funcionarios = new ArrayList<>();
 
-
-    }
-
-
-
-
+	    try {
+	        String linha = ""; 
+	        
+	        while ((linha = br.readLine()) != null) { 
+	            String[] funcionarioVetor = linha.split(",");
+	            
+	            
+	            if (funcionarioVetor.length >= 4) { 
+	                Funcionario funcionario = new Funcionario();
+	                funcionario.setMatricula(funcionarioVetor[0]);
+	                funcionario.setNome(funcionarioVetor[1]);
+	                funcionario.setCargo(funcionarioVetor[2]);
+	                funcionario.setSetor(funcionarioVetor[3]); 
+	                funcionarios.add(funcionario);
+	            } else {
+	               
+	                System.err.println("Linha malformada ignorada: " + linha);
+	            }
+	        }
+	    } catch (Exception e) {
+	        
+	        System.err.println("Erro ao ler dados dos funcionários: " + e.getMessage());
+	        e.printStackTrace();
+	    } 
+	    
+	    
+	    return funcionarios; 
+	}
 }
